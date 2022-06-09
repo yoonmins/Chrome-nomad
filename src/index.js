6dd5de1291506b2008180ce0a25ab8e683da2f4f
SungMin Yoon <yoonmin.tech@gmail.com>
2022-06-09 13:43:29 +0900

    0609 code challenge

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+let app = document.getElementById("app");
+
+// app.addEventListener(
+//   "mouseenter",
+//   function (event) {
+//     event.target.style.color = "purple";
+//     app.innerHTML = "The Mouse is here!";
+//   },
+//   false
+// );
+
+// app.addEventListener(
+//   "mouseleave",
+//   function (event) {
+//     event.target.style.color = "skyblue";
+//     app.innerHTML = "The Mouse is over!";
+//   },
+//   false
+// );
+
+function handleMoseover() {
+  app.style.color = "purple";
+  app.innerHTML = "The Mouse is here!";
+}
+
+function handleMoseleave() {
+  app.style.color = "skyblue";
+  app.innerHTML = "The Mouse is over!";
+}
+
+function handleWindowContextmenu() {
+  app.style.color = "red";
+  app.innerHTML = "That was a right click";
+}
+
+function handleWindowResize() {
+  app.style.color = "green";
+  app.innerHTML = "You just resized";
+}
+
+app.addEventListener("mouseover", handleMoseover);
+app.addEventListener("mouseleave", handleMoseleave);
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("contextmenu", handleWindowContextmenu);
