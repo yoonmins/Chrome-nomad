11c755b55b4511f971e83f05c8399dea37337273
SungMin Yoon <yoonmin.tech@gmail.com>
2022-06-10 23:57:56 +0900

    노마드 코더 코드챌린지 5일차 수정 1

--- src/index.js
+++ src/index.js
@@ -1,46 +1,22 @@
 import "./styles.css";
 
-let app = document.getElementById("app");
+function handleResize() {
+  let resizeWidth = document.body.offsetWidth;
+  let resizeColor = document.body.style.backgroundColor;
+  // let resizeGreenColor = (document.body.style.backgroundColor = "green");
+  // let resizeSkyblueColor = (document.body.style.backgroundColor = "skyblue");
+  // let resizeRedColor = (document.body.style.backgroundColor = "red");
 
-// app.addEventListener(
-//   "mouseenter",
-//   function (event) {
-//     event.target.style.color = "purple";
-//     app.innerHTML = "The Mouse is here!";
-//   },
-//   false
-// );
-
-// app.addEventListener(
-//   "mouseleave",
-//   function (event) {
-//     event.target.style.color = "skyblue";
-//     app.innerHTML = "The Mouse is over!";
-//   },
-//   false
-// );
-
-function handleMoseover() {
-  app.style.color = "purple";
-  app.innerHTML = "The Mouse is here!";
+  // if (document.body.classList.contains(resizeGreenColor)) {
+  //   document.body.classList.remove(resizeRedColor);
+  // } else {
+  //   document.body.classList.add(resizeSkyblueColor);
+  // }
+  if (resizeWidth < 300) {
+    resizeColor = "green";
+  } else {
+    resizeColor = "blue";
+  }
 }
 
-function handleMoseleave() {
-  app.style.color = "skyblue";
-  app.innerHTML = "The Mouse is over!";
-}
-
-function handleWindowContextmenu() {
-  app.style.color = "red";
-  app.innerHTML = "That was a right click";
-}
-
-function handleWindowResize() {
-  app.style.color = "green";
-  app.innerHTML = "You just resized";
-}
-
-app.addEventListener("mouseover", handleMoseover);
-app.addEventListener("mouseleave", handleMoseleave);
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("contextmenu", handleWindowContextmenu);
+window.addEventListener("resize", handleResize);
