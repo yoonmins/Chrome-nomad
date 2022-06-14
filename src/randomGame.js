a045f5752a6573cab986c202f3c8ec3c810b2222
SungMin Yoon <yoonmin.tech@gmail.com>
2022-06-14 23:49:29 +0900

    랜덤 숫자 게임

--- src/randomGame.js
+++ src/randomGame.js
@@ -1,1 +1,38 @@
 import "./styles.css";
+
+// const GmaeChose = document.querySelector(".Game-chose");
+const GameOutput = document.querySelector(".Game-output");
+const ChoseNumber = document.querySelector("#Game-input-number").value;
+const GuessNumber = document.querySelector("#Game-guess").value;
+
+const GameResult = document.querySelector(".Game-result");
+
+// const RandomNumberOutput = Math.floor(Math.random() * ChoseNumber);
+
+function RandomNumberOutput() {
+  document.querySelector("#chose-number").innerText = ChoseNumber;
+}
+
+function ChoseGuessNumber() {
+  document.querySelector("#random-number").innerText = GuessNumber;
+}
+
+document
+  .querySelector(".Game-guess-button")
+  .addEventListener("click", function OutputNumber(event) {
+    document.querySelector(".random-number").innerHTML =
+      "You chose : " +
+      ChoseGuessNumber +
+      ",the machine chose : " +
+      RandomNumberOutput +
+      ".";
+  });
+
+function Result() {
+  if (ChoseNumber === GuessNumber) {
+    ("You won!");
+  } else {
+    ("You lost!");
+  }
+}
+GameResult.innerHTML = Result();
