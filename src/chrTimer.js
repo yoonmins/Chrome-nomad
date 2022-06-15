cc0c0fcbd75280ef8d1e468290ba1769be32a262
SungMin Yoon <yoonmin.tech@gmail.com>
2022-06-15 23:56:31 +0900

    크리스마스 타이머

---
+++ src/chrTimer.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+const Clock = document.querySelector("strong#Clock");
+
+function getClock() {
+  const nowDate = new Date();
+  const christmasDate = new Date("2022-12-25");
+  const Timer = christmasDate - nowDate;
+
+  const TimerDay = Math.floor(Timer / (1000 * 60 * 60 * 24));
+  const TimerHour = Math.floor((Timer / (1000 * 60 * 60)) % 24);
+  const TimerMin = Math.floor((Timer / (1000 * 60)) % 60);
+  const TimerSec = Math.floor((Timer / 1000) % 60);
+
+  Clock.innerText = `${TimerDay}d ${TimerHour}h ${TimerMin}m ${TimerSec}s`;
+}
+
+getClock();
+setInterval(getClock, 1000);
