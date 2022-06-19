0de12508b7051ce6acfc29394022ac3c3f9f4fe1
SungMin Yoon <yoonmin.tech@gmail.com>
2022-06-20 02:16:51 +0900

    style

--- Chrom/background.js
+++ Chrom/background.js
@@ -1,17 +1,17 @@
-// const imges = [
-//   "earth.jpg",
-//   "wave2.jpg",
-//   "wave.jpg",
-//   "space.jpeg"
-// ];
+const imges = [
+  "https://i.pinimg.com/564x/e1/bc/59/e1bc59d81816ece69a9e8eb636403a74.jpg"
+  // "wave2.jpg",
+  // "wave.jpg",
+  // "space.jpeg"
+];
 
-// const BG = "background";
+const BG = "background";
 
-// const backgroundImg = imges[Math.floor(Math.random() * imges.length)];
+const backgroundImg = imges[Math.floor(Math.random() * imges.length)];
 
-// const bgImges = document.createElement("img");
+const bgImges = document.createElement("img");
 
-// bgImges.className = BG;
-// bgImges.src = `img/${backgroundImg}`;
+bgImges.className = BG;
+bgImges.src = `src=${backgroundImg}`;
 
-// document.body.appendChild(bgImges);
+document.body.appendChild(bgImges);
