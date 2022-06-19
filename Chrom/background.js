cc45b0a5b607947bfa1fdd34e9ffd0c0f0eab633
SungMin Yoon <yoonmin.tech@gmail.com>
2022-06-19 23:50:48 +0900

    Set basckground image

--- Chrom/background.js
+++ Chrom/background.js
@@ -1,19 +1,19 @@
 import "./styles.css";
 
-// const imges=[
-//   // "earth.jpg",
-//   "moun.jpg",
-//   "wave.jpg",
-// 	"sea.jpeg"
-// ];
+const imges = [
+  // "earth.jpg",
+  "wave2.jpg",
+  "wave.jpg",
+  "space.jpeg"
+];
 
-// const BG = "background";
+const BG = "background";
 
-// const backgroundImg = imges[Math.floor(Math.random() * imges.length)];
+const backgroundImg = imges[Math.floor(Math.random() * imges.length)];
 
-// const bgImges = document.createElement("img");
+const bgImges = document.createElement("img");
 
-// bgImges.className = BG;
-// bgImges.src = `img/${backgroundImg}`;
+bgImges.className = BG;
+bgImges.src = `img/${backgroundImg}`;
 
-// document.body.appendChild(bgImges);
+document.body.appendChild(bgImges);
